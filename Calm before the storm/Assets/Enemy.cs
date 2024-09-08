99fafa960992ded3f56dee01ad4ab1b7ffb7e1f8
Jose Quintana Martin <jquintanamartin@hotmail.com>
2024-09-08 18:29:22 -0300

    Enemy attacks player

--- Calm before the storm/Assets/Enemy.cs
+++ Calm before the storm/Assets/Enemy.cs
@@ -7,6 +7,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    [SerializeField] private float attackRate;
+
+    private bool canAttack;
+    private float attackTimer;
+    private float nextAttackTime;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +23,46 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 dir = target.transform.position - transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (target != null)
+        {
+            Vector2 dir = target.transform.position - transform.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-        dir.Normalize();
 
-        rb.velocity = dir * speed;
-        transform.eulerAngles = new Vector3(0, 0, angle);
+            if (canAttack == false)
+            {
+                rb.velocity = Vector2.zero;
+
+                attackTimer += Time.deltaTime;
+                if (attackTimer >= nextAttackTime)
+                {
+                    canAttack = true;
+                }
+            }
+            else 
+            {
+                dir.Normalize();
+
+                rb.velocity = dir * speed;
+            }
+
+            transform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (canAttack == true)
+        {
+            Health healthComponent = collision.gameObject.GetComponent<Health>();
+            if (healthComponent != null)
+            {
+                healthComponent.TakeDamage(damage);
+
+                canAttack = false;
+                nextAttackTime = 1 / attackRate;
+                attackTimer = 0;
+            }
+        }
     }
 }
