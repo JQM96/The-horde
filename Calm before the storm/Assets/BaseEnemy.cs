2cc8998b25180f93ab63de83ac675f338cc10603
JQM96 <jquintanamartin.games@gmail.com>
2025-06-18 06:14:34 -0300

    Added base enemy

---
+++ Calm before the storm/Assets/BaseEnemy.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(Pathfinder))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class BaseEnemy : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform; //Temporal
+
+    Health healthComponent;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        healthComponent = GetComponent<Health>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+}
